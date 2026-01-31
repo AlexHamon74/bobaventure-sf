4a6c31582b23fc879e23d7d21f87142946fd5d8a
alex <alex.hmn18@gmail.com>
2026-01-31 19:31:41 +0100

    feat: add modif profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfilType;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfilController extends AbstractController
 {
-    #[Route('/profil', name: 'app_profil')]
-    public function index(): Response
+    #[Route('/profil/{id}', name: 'app_profil')]
+    public function index(User $user, EntityManagerInterface $em, Request $request): Response
     {
+        // Vérifie qu'il sagit bien de l'utilisateur connecté
+        if ($this->getUser() !== $user) {
+            $currentUser = $this->getUser();
+            if ($currentUser instanceof User) {
+                return $this->redirectToRoute('app_profil', ['id' => $currentUser->getId()]);
+            }
+        }
+
+        $form = $this->createForm(ProfilType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setCreatedAt(new DateTimeImmutable());
+            $em->persist($user);
+            $em->flush();
+            $this->addFlash('success', 'Votre profil à bien été édité');
+            return $this->redirectToRoute('app_profil', [
+                'id' => $user->getId(),
+            ]);
+        }
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'controller_name' => 'Bob-Aventure - Mon Profil',
+            'user' => $user,
+            'form' => $form
         ]);
     }
 }
---
+++ src/Form/ProfilFormType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfilType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('phone_number', TelType::class, [
+                'label' => 'Téléphone',
+                'required' => false,
+            ])
+            ->add('adress', TextType::class, [
+                'label' => 'Adresse',
+                'required' => false,
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Bio',
+                'required' => false,
+                'attr' => [
+                    'rows' => 4,
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer les modifications',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'attr' => [
+                'class' => 'row',
+            ],
+        ]);
+    }
+}
