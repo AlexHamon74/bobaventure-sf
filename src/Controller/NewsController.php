0ab544d5eda4d1490c449f70472710e94e54f4b6
alex <alex.hmn18@gmail.com>
2026-02-08 17:22:37 +0100

    feat: affichage dynamique page Actualités

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class NewsController extends AbstractController
 {
     #[Route('/news', name: 'app_news')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
+        $articles = $articleRepository->findAll();
+
         return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
+            'title' => 'Bob-Aventure - Blog',
+            'articles' => $articles,
         ]);
     }
 }
