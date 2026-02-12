8f0103e483d708c11a2f65ad42570f5405ebd58e
alex <alex.hmn18@gmail.com>
2026-02-12 19:38:55 +0100

    fix name class

--- src/Form/ProfilFormType.php
+++ src/Form/ProfilFormType.php
@@ -12,7 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProfilType extends AbstractType
+class ProfilFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
