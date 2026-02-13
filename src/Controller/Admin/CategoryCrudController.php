e23b3867ad475f63c32a41451adbf12483718854
alex <alex.hmn18@gmail.com>
2026-02-13 19:20:37 +0100

    feat: amélioration categoryCrudController easyadmin

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CategoryCrudController extends AbstractCrudController
@@ -18,8 +20,16 @@ class CategoryCrudController extends AbstractCrudController
     {
         return [
             TextField::new('name')
-            ->setLabel('Nom de la catégorie')
-            ->setRequired(true),
+                ->setLabel('Nom de la catégorie')
+                ->setRequired(true),
+
+            IntegerField::new('articles')
+                ->setLabel('Nombre d\'articles')
+                ->onlyOnIndex()
+                ->setVirtual(true) 
+                ->formatValue(function ($value, $entity) {
+                    return $entity->getArticles()->count();
+                }),
         ];
     }
 
@@ -27,5 +37,17 @@ class CategoryCrudController extends AbstractCrudController
     {
         return $crud
             ->setPageTitle(Crud::PAGE_INDEX, 'Gestion des catégories');
+    }
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if (!$entityInstance->getArticles()->isEmpty()) {
+            $this->addFlash(
+               'danger',
+               'Impossible de supprimer cette catégorie car elle contient des articles.'
+            );
+            return;
+        }
+        parent::deleteEntity($entityManager, $entityInstance);
     }
 }
