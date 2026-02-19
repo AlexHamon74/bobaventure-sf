d7f7b6e074067762782ce9ba582ef4f01ad25dd7
alex <alex.hmn18@gmail.com>
2026-02-19 20:07:34 +0100

    feat: ajout recherche et filtre liste articles

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -4,24 +4,33 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class NewsController extends AbstractController
 {
     #[Route('/news', name: 'app_news')]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(Request $request, ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response
     {
-        $articles = $articleRepository->findAll();
+        $search = $request->query->get('q');
+        $categoryId = $request->query->get('category');
+
+        $articles = $articleRepository->findByFilters($search, $categoryId);
+        $categories = $categoryRepository->findAll();
 
         return $this->render('news/index.html.twig', [
             'title' => 'Bob-Aventure - Blog',
             'articles' => $articles,
+            'categories' => $categories,
+            'currentSearch' => $search,
+            'currentCategory' => $categoryId,
         ]);
     }
 
-    #[Route('/news/{id}', name:'app_news_details')]
+    #[Route('/news/{id}', name: 'app_news_details')]
     public function details(Article $article): Response
     {
         return $this->render('news/details.html.twig', [
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -16,28 +16,23 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-    //    /**
-    //     * @return Article[] Returns an array of Article objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('a.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findByFilters(?string $search, ?int $categoryId): array
+    {
+        $qb = $this->createQueryBuilder('a')
+            ->leftJoin('a.category', 'c')
+            ->addSelect('c')
+            ->orderBy('a.published_at', 'DESC');
 
-    //    public function findOneBySomeField($value): ?Article
-    //    {
-    //        return $this->createQueryBuilder('a')
-    //            ->andWhere('a.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        if ($search) {
+            $qb->andWhere('a.title LIKE :search OR a.description LIKE :search')
+                ->setParameter('search', '%' . $search . '%');
+        }
+
+        if ($categoryId) {
+            $qb->andWhere('c.id = :categoryId')
+                ->setParameter('categoryId', $categoryId);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
 }
